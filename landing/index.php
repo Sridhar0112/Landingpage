d350fd0dc753d5b73580f3111cfc64e144d1433d
SRIDHAR  S <156069624+Sridhar0112@users.noreply.github.com>
2024-01-09 14:52:58 +0530

    Add files via upload

---
+++ landing/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Shop Dashboard</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <header>
+        <b><h1>ONLINE BOOK SHOP</h1></b>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="home.html">HOME</a></li>
+            <li><a href="book.html">BOOK STORE</a></li>
+            <li><a href="login.html">CART</a></li>
+            <li><a href="about.html">LOGIN</a></li>
+        </ul>
+    </nav>
+    <div class="dashboard-image">
+            <img src="book.jpg" alt="Dashboard Image">
+        </div>
+        <?php include 'footer.html'; ?>
+</body>
+</html>
